2624ea3c0a39bc545a5773e4d75184cec0096e3a
kingsley Nwanesi <62988451+soft-005@users.noreply.github.com>
2025-07-22 16:20:50 +0100

    Update AppServiceProvider.php
    after migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        //View::share(['categories' => Category::all(), 'appInfo' => Setting::first()]);
+        View::share(['categories' => Category::all(), 'appInfo' => Setting::first()]);
     }
 }
